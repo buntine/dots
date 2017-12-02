c35ef79b2f3b25abdf20283938b297efa9be5415
Andrew Buntine <info@bunts.io>
2017-12-02 18:45:44 +1100

    Removed update-state

--- src/dots/core.clj
+++ src/dots/core.clj
@@ -2,29 +2,24 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+(defn build [state]
+  160)
+
 (defn setup []
   (q/frame-rate 30)
   (q/color-mode :hsb)
-  {:color 0
-   :cells 30
-   :rows 30
-   :min-rad 4
-   :max-rad 16
-   :cell-size 22
-   :angle 0})
+  {:table (build 1)})
 
-(defn update-state [state]
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+(defn update-state [s] s)
 
 (defn draw-state [state]
   (q/no-loop)
-  (q/background 240)
-  (q/fill (:color state) 255 255)
-      (q/ellipse 100 100 100 100))
+  (q/background 255)
+  (q/fill 100 255 255)
+    (q/ellipse 100 (:table state) 100 100))
 
 (q/defsketch dots
-  :title "You spin my circle right round"
+  :title "Dots"
   :size [820 820]
   :setup setup
   :update update-state
