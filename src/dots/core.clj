708ffed7b4aa92e8929a35d70e3a2c434895e9a9
Andrew Buntine <info@bunts.io>
2017-12-03 23:05:03 +1100

    Generating state for rols and cols

--- src/dots/core.clj
+++ src/dots/core.clj
@@ -2,14 +2,40 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+(defn build-cell [x y prev-radius]
+  (let [radius 4]
+    [x
+     (- y 4
+          (/ radius 2)
+          (/ prev-radius 2))
+     radius]))
+
+(defn build-col [cells total threshold]
+  (if (> total threshold)
+    cells
+    (let [cell (apply build-cell (last cells))]
+      (recur
+        (conj cells cell)
+        (+ total (last cell))
+        threshold))))
+
 (defn build [state]
-  [[[100, 656, 8], [100, 646, 4], [100, 634, 12]]
-   [[122, 658, 4], [122, 650, 4], [122, 640, 8]]])
+  (for [col (range (:cols state))]
+    (let [offset (* col (:col-size state))]
+      (build-col [(+ 100 offset) 658 4]
+                 0
+                 (:max-height state)))))
+  ;[[[100, 658, 4], [100, 648, 8], [100, 634, 12]]
+  ; [[122, 658, 4], [122, 650, 4], [122, 640, 8]]])
 
 (defn setup []
   (q/frame-rate 30)
   (q/color-mode :hsb)
-  {:table (build 1)})
+  {:table
+    (build
+      {:cols 30
+       :max-height 600
+       :col-size 22})})
 
 (defn update-state [s] s)
 
@@ -17,7 +43,7 @@
   (q/no-loop)
   (q/no-stroke)
   (q/background 255)
-  (q/fill 100 100 100)
+  (q/fill 0 0 50)
 
   (doseq [col (:table state)
           cell col]
