90f3f9cdd6a25e99d054f056f3168743f61fb4ab
Andrew Buntine <info@bunts.io>
2017-12-06 23:27:19 +1100

    Added checksum to footer, Refactoring

--- src/dots/core.clj
+++ src/dots/core.clj
@@ -1,21 +1,25 @@
 (ns dots.core
   (:require [quil.core :as q]
             [quil.middleware :as m]
+            [clojure.string :as s]
             [clojure.java.io :as io]))
 
 (def delta 1)
 (def min-rad 6)
 (def max-rad 8)
 (def padding 4)
-(def cols 24)
+(def cols 26)
 (def min-y 70)
 (def y-variance 250)
 (def height 1000)
-(def xy-padding 100)
+(def xy-padding 75)
+(def checksum-padding (* delta 30))
 (def col-size 22)
 (def window-width 702)
 (def window-height 993)
-(def bg 240)
+(def bg 231)
+(def checksum-fg 80)
+(def checksum-size (* delta 14))
 
 (defn build-cell [x y prev-radius]
   (let [radius (max min-rad (* (rand-int max-rad) 2))]
@@ -51,9 +55,25 @@
     6 [156 212 238]
     8 [134 138 170]
     10 [177 73 65]
-    12 [38 30 17]
+    12 [61,102,61]
     14 [166 181 187]
     [150 150 200]))
+
+(defn checksum [data]
+  (* 1200
+    (reduce
+      (fn [acc x]
+        (reduce
+          (fn [acc y]
+            (reduce + acc y)) acc x)) 0 data)))
+
+(defn draw-dots [data]
+  (let [cs (int (checksum data))]
+    (doseq [col data
+            cell col]
+      (let [[x y rad] (map #(* % delta) cell)]
+        (apply q/fill (color-for (last cell)))
+        (q/ellipse x y rad rad)))))
 
 (defn setup []
   (q/frame-rate 29)
@@ -70,17 +90,22 @@
   (q/no-stroke)
   (q/background bg)
 
-  (doseq [col (:table state)
-          cell col]
-    (let [[x y rad] (map #(* % delta) cell)]
-      (apply q/fill (color-for (last cell)))
-      (q/ellipse x y rad rad)))
+  (draw-dots (:table state))
 
-  (let [epoch  (int (/ (System/currentTimeMillis) 1000))
-        path (str "./out/" epoch)]
-    (spit (str path ".txt")
-          (with-out-str (pr (:table state))))
-    (q/save (str path ".tif"))))
+    (let [w (* delta window-width)
+          h (* delta window-height)]
+      (q/fill checksum-fg)
+      (q/text-align :center :bottom)
+      (q/text-size checksum-size)
+      (q/text
+        (s/upper-case (format "%x" cs))
+        0 0 w (- h checksum-padding)))
+
+    (let [epoch  (int (/ (System/currentTimeMillis) 1000))
+          path (str "./out/" epoch)]
+      (spit (str path ".txt")
+            (with-out-str (pr data)))
+      (q/save (str path ".tif")))))
 
 (q/defsketch dots
   :title "Dots"
